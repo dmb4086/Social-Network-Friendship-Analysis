fdbde7a863f171b3ec7af8fd687879379527117c
Dev <dmb4086@rit.edu>
2020-05-01 02:08:33 -0400

    improved comments for better readability

--- src/Analysis.java
+++ src/Analysis.java
@@ -6,40 +6,49 @@ import java.util.*;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
+/**
+ * The type Analysis.
+ *
+ * @AUTHOR: Dev Bhatt
+ */
 public class Analysis {
 
-    public static HashMap<Integer,Integer> UserMap = new HashMap<Integer, Integer>();
+    /**
+     * The constant UserMap.
+     */
+    public static HashMap<Integer, Integer> UserMap = new HashMap<Integer, Integer>();
+    private static int Totalusers = 0;
 
-
-    public static void PoupulateUserBase(String filepath ) throws FileNotFoundException {
-
-
-        int Totalusers = 0;
-
+    /**
+     * Poupulate user base.
+     *
+     * @param filepath the filepath
+     * @throws FileNotFoundException the file not found exception
+     */
+    public static void PoupulateUserBase(String filepath) throws FileNotFoundException {
 
         try {
             File file = new File(filepath);
             FileInputStream inputStream = new FileInputStream(file);
             Scanner scanner = new Scanner(inputStream, StandardCharsets.UTF_8);
 
-            Totalusers = Integer.parseInt(scanner.nextLine()) ;
+            Totalusers = Integer.parseInt(scanner.nextLine());
 
-            while (scanner.hasNextLine()){
+            while (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
 //                System.out.println(line);
 
                 String number[] = line.split(" ");
                 int key = Integer.parseInt(String.valueOf(number[0]));
 
-                if (UserMap.containsKey(key)){
-                    UserMap.put(key,UserMap.get(key)+1);
-                }
-                else {
+                if (UserMap.containsKey(key)) {
+                    UserMap.put(key, UserMap.get(key) + 1);
+                } else {
                     UserMap.put(key, 1);
                 }
             }
 
-            System.out.println("Total Users: "+ Totalusers);
+            System.out.println("Total Users: " + Totalusers);
 //            System.out.println(UserMap);
 
 
@@ -50,12 +59,14 @@ public class Analysis {
 
     }
 
-    /*
-        As the HashMap is defined as <User, Friendships>
-        all this method does is sort the HashMap by descending order of values
-        and returns a list of top 10 users.
+    /**
+     * As the HashMap is defined as <User, Friendships>
+     * all this method does is sort the HashMap by descending order of values
+     * and returns a list of top 10 users.
+     *
+     * @param map the map
      */
-    public static void TopTenFriends(HashMap<Integer,Integer> map){
+    public static void TopTenFriends(HashMap<Integer, Integer> map) {
 
         LinkedHashMap<Integer, Integer> reverseSortedMap = new LinkedHashMap<>();
         map.entrySet().stream().sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
@@ -63,29 +74,45 @@ public class Analysis {
 
 
         // change this to only print top 10 instead of all
-        System.out.println("TOP 10 USERS");
-        int counter = 0 ;
-        for (Map.Entry<Integer, Integer> entry : reverseSortedMap.entrySet()){
-            if (counter == 9){
+        System.out.println("TOP 10 Twitch user friend counts");
+        int counter = 0;
+        for (Map.Entry<Integer, Integer> entry : reverseSortedMap.entrySet()) {
+            if (counter == 9) {
                 break;
-            }
-            else
-                System.out.println("User = " + entry.getKey() +
-                        " Friends = " + entry.getValue());
-                counter++;
+            } else
+                System.out.println("User:  " + entry.getKey() +
+                        ", Friend Count: " + entry.getValue());
+            counter++;
 
-    }
+        }
         System.out.println(reverseSortedMap);
 
-}
+    }
 
+    /**
+     * This function simply computes the average friends of all users
+     *
+     * @param UserBase the user base
+     * @return average : the average number of friends of all users
+     */
+    public static int AverageFriendCount(HashMap<Integer, Integer> UserBase) {
+
+        // take in the hashmap, add all the values and divide the computed value by total users
+
+        return 0;
+    }
+
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     * @throws FileNotFoundException the file not found exception
+     */
     public static void main(String[] args) throws FileNotFoundException {
         long startTime = System.nanoTime();
 
-        PoupulateUserBase("/Users/dev/Documents/GitHub/Social Network Friendship Analysis/twitch_friendships.txt");
+        PoupulateUserBase("/Users/dev/Documents/GitHub/Social Network Friendship Analysis/friendships2.txt");
         TopTenFriends(UserMap);
-
-
 
 
         long endTime = System.nanoTime();
@@ -94,24 +121,6 @@ public class Analysis {
 //        System.out.println(durationInNano);
         System.out.println(durationInMillis);
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 }
